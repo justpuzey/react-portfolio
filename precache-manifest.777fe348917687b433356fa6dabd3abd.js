efa7f231c5788f11462da743b75f58e052e78064
Justin Puzey <justpuzey@gmail.com>
2021-07-11 22:47:18 -0400

    Updates

---
+++ precache-manifest.777fe348917687b433356fa6dabd3abd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8977b45729149e6b32720208643bde06",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c62e59db544730bce54b",
+    "url": "/react-portfolio/static/css/main.c876a1d5.chunk.css"
+  },
+  {
+    "revision": "7d39d1d182c86510409b",
+    "url": "/react-portfolio/static/js/2.ae406b3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.ae406b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62e59db544730bce54b",
+    "url": "/react-portfolio/static/js/main.3fdba17a.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "8a3310a8da86c86cd682b4c89bb97abb",
+    "url": "/react-portfolio/static/media/Justin_Puzey.8a3310a8.png"
+  },
+  {
+    "revision": "301f2e9adfc98dc3eb1aab7b1593c4fc",
+    "url": "/react-portfolio/static/media/date-nite.301f2e9a.png"
+  },
+  {
+    "revision": "11a6f0a68fbba80360d8bf70015def7e",
+    "url": "/react-portfolio/static/media/overflow.11a6f0a6.png"
+  },
+  {
+    "revision": "de7649eb1d746d8d56664821f20097ac",
+    "url": "/react-portfolio/static/media/run-buddy.de7649eb.png"
+  },
+  {
+    "revision": "2fb38afea54dd0e460186ee974e7949a",
+    "url": "/react-portfolio/static/media/water-ways.2fb38afe.png"
+  }
+]);
